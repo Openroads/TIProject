50c0a5598772ecd0c833c868c23996fd4be57b9e
Piotr Makowiec <piotrmakowiec@outlook.com>
2018-10-08 01:01:51 +0200

    Created basic form with alert message with name (without contacting rest djiango api)

--- ReactClient/reactapplication/src/App.js
+++ ReactClient/reactapplication/src/App.js
@@ -3,22 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event){
+    alert('Your name is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Urszula Lis, Dariusz Szyszlak, Piotr Makowiec
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="name" placeholder="Enter your name..." value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         </header>
       </div>
     );
