a7083ad8302c15cec2111b125662204c0a43cf28
Piotr Makowiec <piotrmakowiec@outlook.com>
2018-10-11 00:30:18 +0200

    Navigation - Footer - Login Page

---
+++ ReactClient/reactapplication/src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Document application</h1>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mollis pulvinar lobortis. Etiam consectetur sodales odio, at cursus augue ultricies vel. Cras ac ex neque. Sed condimentum pulvinar commodo. Nulla sed lacus semper, aliquet ex sit amet, pharetra metus. Vivamus fringilla, purus fermentum euismod viverra, libero nunc tristique odio, eget elementum sem diam hendrerit urna. Nam vestibulum dapibus aliquam. Donec finibus sollicitudin dolor, vel dictum ex mollis eget. Nullam ac nisl in nunc venenatis bibendum. In cursus posuere ligula, eu pellentesque magna tempor a. Mauris a ligula vel nisl congue varius molestie sit amet ligula. Vivamus tempus sapien pretium nulla mattis, non facilisis metus volutpat. Ut posuere lacinia diam, et imperdiet turpis tristique nec. Suspendisse mattis ac tortor nec accumsan. Curabitur vel fringilla tellus, at mattis orci.
+
+Vestibulum a nulla neque. Nunc sed convallis mi, et laoreet odio. Sed condimentum, ligula sed efficitur facilisis, nulla purus pellentesque ante, nec molestie neque felis eget erat. Donec dignissim, metus tempus varius hendrerit, turpis dolor iaculis neque, non tincidunt elit odio vel metus. Suspendisse placerat venenatis suscipit. Donec elementum enim quis neque vehicula laoreet. Nullam sit amet pharetra nisi, vel vulputate lorem.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
