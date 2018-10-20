3e04f6dbf7c217e9cd2e480199147a2d6964631c
daren <daren22@o2.pl>
2018-10-20 22:13:47 +0200

    Add lock/unlock document

--- djangoREST/onlineDocumentEdit/urls.py
+++ djangoREST/onlineDocumentEdit/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from onlineDocumentEdit import views
-from onlineDocumentEdit.views import ListDocumentsView
+from onlineDocumentEdit.views import ListDocumentsView, DocumentLockView, DocumentUnlockView
 from onlineDocumentEdit.views import DocumentsDetailView
 
 urlpatterns = [
 	path('documents/', ListDocumentsView.as_view()),
 	path('document/<int:pk>/', DocumentsDetailView.as_view()),
+	path('document/<int:documentId>/editing-by/<int:userId>/', DocumentLockView.as_view()),
+	path('document/<int:documentId>/stop-editing/', DocumentUnlockView.as_view()),
 	path('users/', views.UserList.as_view()),
 	path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
