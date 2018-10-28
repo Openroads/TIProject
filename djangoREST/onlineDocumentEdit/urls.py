79809df0a1a87fd4b93bcdc07df18875e6fc6f06
daren <daren22@o2.pl>
2018-10-28 13:46:52 +0100

    Add login endpoint with json example: { 	"username":"username", 	"password":"pasword123" }

--- djangoREST/onlineDocumentEdit/urls.py
+++ djangoREST/onlineDocumentEdit/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from onlineDocumentEdit import views
 from onlineDocumentEdit.views import ListDocumentsView, DocumentLockView, DocumentUnlockView
@@ -11,4 +12,5 @@ urlpatterns = [
 	path('document/<int:documentId>/stop-editing/', DocumentUnlockView.as_view()),
 	path('users/', views.UserList.as_view()),
 	path('users/<int:pk>/', views.UserDetail.as_view()),
+	path('users/login/', csrf_exempt(views.UserLogin.as_view())),
 ]
