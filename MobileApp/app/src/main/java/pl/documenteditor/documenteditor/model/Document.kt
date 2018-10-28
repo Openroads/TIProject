1d8f19424afca6fa2f68e905d58cb897aafeb739
urszulalis <urszula.lis95@gmail.com>
2018-10-28 15:20:28 +0100

    add new modules

---
+++ MobileApp/app/src/main/java/pl/documenteditor/documenteditor/model/Document.kt
@@ -0,0 +1,5 @@
+package pl.documenteditor.documenteditor.model
+
+class Document(val id: Int, val title:String, val content:String) {
+
+}
---
+++ MobileApp/app/src/main/java/pl/documenteditor/documenteditor/utils/Constants.kt
@@ -0,0 +1,9 @@
+package pl.documenteditor.documenteditor.utils
+
+class Constants {
+
+    companion object {
+        const val REST_SERVERS_ADDRESS = "http://10.0.2.2:8000/"
+    }
+
+}
