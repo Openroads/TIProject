05571e591dd7a1629544966c57b2a5f86e7ec53c
daren <daren22@o2.pl>
2018-10-31 23:14:16 +0100

    Add navigation to main activity and show current user name in.

--- MobileApp/app/src/main/java/pl/documenteditor/documenteditor/LoginActivity.kt
+++ MobileApp/app/src/main/java/pl/documenteditor/documenteditor/LoginActivity.kt
@@ -1,36 +1,37 @@
 package pl.documenteditor.documenteditor
 
+import android.Manifest.permission.READ_CONTACTS
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.app.LoaderManager.LoaderCallbacks
 import android.content.CursorLoader
+import android.content.Intent
 import android.content.Loader
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
 import android.widget.TextView
-
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
-import android.content.Intent
 import com.google.gson.Gson
-
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 import pl.documenteditor.documenteditor.model.User
-
-import okhttp3.*
 import pl.documenteditor.documenteditor.utils.Constants
+import java.util.*
 
 /**
  * A login screen that offers login via email/password.
@@ -270,9 +271,12 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
 
                 val userJson = response.body()?.string()
                 gson.fromJson(userJson, User::class.java)
-            } catch (e: InterruptedException) {
-                null
+
+            } catch (ex: Exception) {
+                Log.e(TAG, "Cant connect to rest api server", ex)
+                return null
             }
+
         }
 
         override fun onPostExecute(user: User?) {
@@ -307,5 +311,6 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
          */
         private const val REQUEST_READ_CONTACTS = 0
         const val USER_DATA = "user_data_object"
+        const val TAG = "ODE_LoginActivity"
     }
 }
--- MobileApp/app/src/main/java/pl/documenteditor/documenteditor/MainActivity.kt
+++ MobileApp/app/src/main/java/pl/documenteditor/documenteditor/MainActivity.kt
@@ -2,29 +2,55 @@ package pl.documenteditor.documenteditor
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.google.gson.GsonBuilder
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.app_bar_main2.*
+import kotlinx.android.synthetic.main.content_main2.*
+import kotlinx.android.synthetic.main.nav_header_main2.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import pl.documenteditor.documenteditor.adapters.DocumentListAdapter
 import pl.documenteditor.documenteditor.model.Document
+import pl.documenteditor.documenteditor.model.User
 import pl.documenteditor.documenteditor.utils.Constants
 
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-class MainActivity : AppCompatActivity() {
+    private var user:User? = User(username = "John",password = "pass")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //val user = intent.getSerializableExtra(LoginActivity.USER_DATA) as User
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(toolbar)
+
+        user = intent.getSerializableExtra(LoginActivity.USER_DATA) as? User ?: user
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
 
         API_button.setOnClickListener {
             val url = Constants.REST_SERVERS_ADDRESS + "online-docs/documents/"
 
             AsyncTaskHandleRestApi().execute(url)
-
         }
     }
 
@@ -55,8 +81,63 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main2, menu)
+        if (user != null) {
+            username_textView.text = user!!.username
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
     companion object {
         const val  TAG :String = "ODE_MainActivity" // ODE - online document editor
     }
-}
 
+}
